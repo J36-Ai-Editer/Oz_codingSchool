cb78d851a259a01ff3d82a2f4e4085f89f2cc0b5
Minjae <kkmj1150@gmail.com>
2026-07-14 15:57:08 +0900

    app 업데이트

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# 모든 회원의 정보를 목록으로 조회하는 API
+# endpoint: practice_api/users
+
+# 회원의 id 값을 path parameter로 입력받아 특정 회원의 정보를 조회하는 API
+# endpoint: practice_api/users/{user_id: int}
+
+# 회원의 정보를 Request Body로 입력받아 user_list에 추가하는 API
+# endpoint: practice_api/users
+
+# 회원의 id 값을 path parameter로 입력받고, 나이와 이메일을 Request Body로 입력받아 user_list에서 해당 회원의 정보를 수정하는 API
+# endpoint: practice_api/users/{user_id: int}
+
+# 회원의 id 값을 path parameter로 입력받아 특정 회원의 정보를 삭제하는 API
+# endpoint: practice_api/users/{user_id: int}
